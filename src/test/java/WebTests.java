ff5c477e3c1a2a6906570993e0ff7e179a9dedbd
Marina <sms2004@yandex.ru>
2022-04-25 23:49:19 +0500

    add WebTests.java

--- src/test/java/WebTests.java
+++ src/test/java/WebTests.java
@@ -2,14 +2,16 @@ import config.WebDriverConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LocalTests {
+public class WebTests {
 
 
     @Test
     public void localTest() {
-        System.setProperty("localOrRemove", "local");
+        System.setProperty("localOrRemote", "local");
 
         WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
         assertThat(config.browserName()).isEqualTo("chrome");
@@ -21,7 +23,7 @@ public class LocalTests {
 
     @Test
     public void remoteTest() {
-        System.setProperty("localOrRemove", "remote");
+        System.setProperty("localOrRemote", "remote");
 
         WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
         assertThat(config.browserName()).isEqualTo("chrome");
@@ -30,4 +32,12 @@ public class LocalTests {
         assertThat(config.baseUrl()).isEqualTo("https://github.com");
         assertThat(config.remoteUrl()).isEqualTo("https://user1:1234@selenoid.autotests.cloud/wd/hub");
     }
+
+    @Test
+     void webTest() {
+        open("https://github.com");
+        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        $$("ul.repo-list").first().shouldHave(text("selenide/selenide"));
+    }
+
 }
